6f45a38bbef5f1417c8fdb9d5de4d64d9e39b1ef
vidhyasagar70 <vidhyas369@gmail.com>
2025-10-25 23:06:59 +0530

    bug fixed

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -280,7 +280,7 @@ export default function Header() {
 
                 {/* Main Navigation Bar */}
 
-                <div className="flex container mx-auto items-center justify-between">
+                <div className="flex container mx-auto px-4 md:px-6 lg:px-8 items-center justify-between">
                     {/* Mobile Menu Button */}
                     <button
                         onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
