365f1ec2fae3cd1f79aaf74b817fe74f55902e5c
vidhyasagar70 <vidhyas369@gmail.com>
2025-11-14 15:24:34 +0530

    bug fixed

--- src/app/limitedEdition/page.tsx
+++ src/app/limitedEdition/page.tsx
@@ -1,12 +1,9 @@
 // app/limitedEdition/page.tsx
 "use client";
-import { useState } from "react";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import DiamondStockTableWithFilter from "@/components/LimitedEdition/DiamondStockTableWithFilterlimited";
 
 export default function LimitedEditionPage() {
-  const [selectedShape, setSelectedShape] = useState<string[]>([]);
-
   return (
     <ProtectedRoute requireAuth={true} allowedRoles={["ADMIN"]} redirectTo="/">
       <DiamondStockTableWithFilter />
