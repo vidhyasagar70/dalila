5a3e43437737d56f4631d89aac9b924cb6e9214f
vidhyasagar70 <vidhyas369@gmail.com>
2025-11-18 10:57:16 +0530

    eror fixed

--- src/components/DiamondStockTable.tsx
+++ src/components/DiamondStockTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useCallback } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import Image from "next/image";
 import {
   ChevronLeft,
